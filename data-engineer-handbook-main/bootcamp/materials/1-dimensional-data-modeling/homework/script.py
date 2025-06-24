9f893ef4ec5f5bfeea49f14554ec1e39891f59c4
Oladipupo kola <komolafe.solomon@yahoo.com>
2025-06-24 23:31:58 +0200

    script.py

--- data-engineer-handbook-main/bootcamp/materials/1-dimensional-data-modeling/homework/script.py
+++ data-engineer-handbook-main/bootcamp/materials/1-dimensional-data-modeling/homework/script.py
@@ -25,3 +25,38 @@ SELECT
 FROM actor_films_raw
 WHERE year <= 2000
 GROUP BY actorid;
+
+CREATE TABLE actors_history_scd (
+    actorid STRING,
+    actor STRING,
+    quality_class STRING,
+    is_active BOOLEAN,
+    start_date DATE,
+    end_date DATE,
+    PRIMARY KEY (actorid, start_date)
+);
+INSERT INTO actors_history_scd
+SELECT
+    actorid,
+    actor,
+    quality_class,
+    is_active,
+    DATE_FROM_UNIX_DATE(MIN(year) * 365) AS start_date,
+    DATE_FROM_UNIX_DATE(MAX(year) * 365) AS end_date
+FROM (
+    SELECT
+        actorid,
+        ANY_VALUE(actor) AS actor,
+        CASE
+            WHEN AVG(rating) > 8 THEN 'star'
+            WHEN AVG(rating) > 7 THEN 'good'
+            WHEN AVG(rating) > 6 THEN 'average'
+            ELSE 'bad'
+        END AS quality_class,
+        MAX(CASE WHEN year = EXTRACT(YEAR FROM CURRENT_DATE()) THEN TRUE ELSE FALSE END) AS is_active,
+        year
+    FROM actor_films_raw
+    GROUP BY actorid, year
+)
+GROUP BY actorid, actor, quality_class, is_active;
+
